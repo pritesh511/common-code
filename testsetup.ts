b379bfd8cc6b6f490d1a0bead4dc2cf1d9b1eeb0
pritesh511 <94275226+pritesh511@users.noreply.github.com>
2025-03-17 10:32:38 +0530

    Update testsetup.ts

--- testsetup.ts
+++ testsetup.ts
@@ -38,6 +38,50 @@ jest.mock("react-native-fbsdk", () => ({
   },
 }));
 
+est.mock('url', () => ({
+    createObjectURL: jest.fn(),
+  }));
+  global.localStorage = {
+    length: 0,
+    clear: jest.fn(),
+    getItem: jest.fn(),
+    key: jest.fn(),
+    removeItem: jest.fn(),
+    setItem: jest.fn()
+}
+const localStorageMock = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  clear: jest.fn()
+};
+global.localStorage = localStorageMock;
+function FormDataMock() {
+  this.append = jest.fn();
+}
+
+jest.mock('../../framework/src/Utilities.ts', () => ({
+  getStorageData: jest.fn().mockImplementation((key) => {
+      if (key === 'loginData') {
+        return Promise.resolve('bgklgkl');
+      }
+      return Promise.resolve(null);
+    }),
+    setStorageData: jest.fn(),
+    removeStorageData: jest.fn(),
+  }));
+
+jest.mock("../../framework/src/StorageProvider", () => {
+    return {
+        get: jest.fn()
+            .mockImplementation((storage) => {
+                return storage === "token" ? Promise.resolve(JSON.stringify(
+                  {"serialized_data":{"data":{"id":626,"type":"email_account"},"meta":{"token":"eyJhbGciOiJIUzUxMiJ9.eyJpZCI6NjI2LCJleHAiOjE3MzAxMjMxODUsInRva2VuX3R5cGUiOiJsb2dpbiJ9.SZKFzgebr8p36oIxYZkMFFjvmEQ5PNTX4vfLKAm3TjY2u6q1u88GWiQehjcYVPw5fcgaCkhJTmPxo0qaPc62YQ","refresh_token":"eyJhbGciOiJIUzUxMiJ9.eyJpZCI6NjI2LCJleHAiOjE3NjE1NzI3ODUsInRva2VuX3R5cGUiOiJyZWZyZXNoIn0.iNtOCO3B3eQngrV9DeTBHJU4iRWzbsHEqvr-X_5q5cwRsmJjF8xuivAOfSJdTBLY__rrpaGe3FofEEav1SDm7w"}},"id":626,"full_name":"lawyer oned","email":"lawadmin1@gmail.com","first_name":"lawyer","last_name":"oned","user_role":"lawfirm_admin"}
+                )) : storage === "token"
+            }),
+        remove: jest.fn(),
+    };
+})
+
 function getMockFunction(request) {
   const { url, headers } = request;
   const token = headers.token;
